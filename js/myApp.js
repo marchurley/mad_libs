2de1edf982804c4000097030f3d23734b0489a20
marchurley <marc@swiss-magic.net>
2016-12-13 11:37:29 +0800

    Add controller and Array for gender

--- js/myApp.js
+++ js/myApp.js
@@ -1,9 +1,8 @@
 angular.module('myApp', [])
-    .constant('VERSION', 1.1)
-    .constant('VERSION2', 2.1)
+    .constant('VERSION', '2.0')
     .run(function(VERSION, $rootScope) { // With run I inject the scope into the rootScope = everywhere
     $rootScope.version = VERSION;
 	})
-    .controller('MyCtrl', function(VERSION2, $scope) { // With controller I use for logical blocks of code to inject on certain tags
-        $scope.version2 = VERSION2;
+    .controller('MyCtrl', function($scope) { // With controller I use for logical blocks of code to inject on certain tags
+        $scope.gender = ['he', 'him', 'his', 'He'];
     }); // ; only after the chaining is finished. No ; before that
