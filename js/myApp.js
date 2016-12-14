1fbcb89382d32f32dd7ba232105aa0575a09986b
Mark Nyon <mark.nyon@runads.com>
2016-12-14 06:25:28 -0500

    Added a skeleton of a proposed radio solution
    - js/myApp.js - Added a proposed solution to the radio button
      challenge

--- js/myApp.js
+++ js/myApp.js
@@ -1,8 +1,14 @@
 angular.module('myApp', [])
     .constant('VERSION', '2.0')
     .run(function(VERSION, $rootScope) { // With run I inject the scope into the rootScope = everywhere
-    $rootScope.version = VERSION;
+	    $rootScope.version = VERSION;
 	})
     .controller('MyCtrl', function($scope) { // With controller I use for logical blocks of code to inject on certain tags
+    	// One solution to this is to have the array for each gender mapped to a singular key, and put the key in the radio
+    	// radio button HTML
+    	$scope.genderHash = {
+    		male: [],
+    		female: []
+    	}
         $scope.gender = ['he', 'him', 'his', 'He'];
     }); // ; only after the chaining is finished. No ; before that
