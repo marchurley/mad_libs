f37a3a41f2903886dff22b37775cc3c545b2c259
marchurley <marc@swiss-magic.net>
2016-12-20 09:29:23 +0800

    Refactoring
    Add ng-click and called function check with argument gender in html
    Declare function check with if statement to see with gender has value male
    or female in controller

--- js/myApp.js
+++ js/myApp.js
@@ -4,17 +4,15 @@ angular.module('myApp', [])
 	    $rootScope.version = VERSION;
 	})
     .controller('MyCtrl', function($scope) { // With controller I use for logical blocks of code to inject on certain tags
-    	// One solution to this is to have the array for each gender mapped to a singular key, and put the key in the radio
-    	// radio button HTML
-    	$scope.genderHash = {
-    		"male": [],
-    		"female": []
-    	}
     	// if statement that changes the array value used based upon the 
     	// value of the model.  The chosenPronouns array will be updated 
-    	// from the scope.genderHash and the finalText element will switch
-    	// back & forth
-    	$scope.chosenPronouns = ['he', 'him', 'his', 'He'];
-    }); // ; only after the chaining is finished. No ; before that
+        this.check = function (gender){
+            if (gender == "male") {
+            this.chosenPronouns = ['he', 'him', 'his', 'He'];
+            } else {
+            this.chosenPronouns = ['she', 'her', 'her', 'She'];
+            }  
+        }
+    });
 
 
